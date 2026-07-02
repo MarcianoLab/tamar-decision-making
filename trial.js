de1f110fa94ca2768ba60af41e71e33f6fb0ae75
Rom Itkin <romitkin@gmail.com>
2026-07-02 13:51:03 +0300

    Move trial config to a seperate file

--- trial.js
+++ trial.js
@@ -46,86 +46,19 @@ function initTrial(qualtricsContext) {
     }
 
     /* ==========================================================
-       IMAGE URL LOOKUP
-       Maps image filenames to their hosted Qualtrics graphic URLs.
+       IMAGE URL LOOKUP  +  TRIAL DATA
+       ----------------------------------------------------------
+       Both are loaded from trial-config.js (window.TRIAL_CONFIG).
+       Make sure trial-config.js is included BEFORE trial.js.
        ========================================================== */
-    var IMG = {
-        "2394_neut.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_5lxgbEa3yaEPMIs",
-        "None.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_4u30N2tqJoyGQHo",
-        "2700_neg.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_uBcToCowu1VT2iL",
-        "2276_neg.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_chGjHXVvAwWrCN2",
-        "2359_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_IlAF9vg4iD1pTPW",
-        "4605_neut.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_Iu4ReFTKEAjN8zh",
-        "6831_neg.bmp": "MISSING",
-        "2305_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_5ULiEyToDlcDKJP",
-        "2026_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_7UiEXEozpIXJlON",
-        "7493_neut.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_qREp8l9C9zQwX3S",
-        "3550_neg.bmp": "MISSING",
-        "2273_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_t3fLwedy3hFjegH",
-        "2749_neut.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_URFHRBm9Ln640as",
-        "6571_neg.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_L0pRWLQA0s4i306",
-        "9400_neg.bmp": "MISSING",
-        "9332_neg.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_NVE4R3ecPnMhCPF",
-        "9419_neg.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_cU2Ps8SRJkKqbZe",
-        "2036_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_x8gKrRccKr2MeV3",
-        "2102_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_CmHmTD0Vt5TScV0",
-        "2107_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_JhKecVuww9vrL09",
-        "2214_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_AeeejmV2an6wQi2",
-        "2308_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_Mb6iTQ0lmP1Q2XJ",
-        "2382_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_BaHbPK2ZmFVawBf",
-        "2390_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_FqFxfOqP0XbMUgx",
-        "2393_neut.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_uZdUsRLWl7qTROT",
-        "2396_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_weI25Rl6nVbImhc",
-        "2397_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_JngF9hpdhze7Dwy",
-        "2411_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_iHqYJTzs5RiD9JH",
-        "2435_neut.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_sm6zePuB3VtBORR",
-        "2488_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_nsHteBGWpPnQ9KZ",
-        "2489_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_5jiXcZy9ZOR4cfl",
-        "2495_neut.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_0QzdylqeJ5zasoS",
-        "2514_neut.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_H7RlkrIEbqpDsh2",
-        "2518_neut.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_U7yRxFnILbtWttt",
-        "2570_neut.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_nTBs1GeduWKkDDD",
-        "2579_neut.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_nMbGzOwWbOdg1Aj",
-        "2580_neut.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_mghwXOX5NcN4PGl",
-        "2593_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_Q0q0IjpEsBy3AWR",
-        "2745.1_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_8jgR4d7kH8mEzVt",
-        "2850_neut.JPG": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_n4vAdlCqBJc3pY0",
-        "8312_neut.jpg": "https://hebrewuni.qualtrics.com/CP/Graphic.php?IM=IM_84VgqilIWvAA8p7"
-    };
+    if (!window.TRIAL_CONFIG) {
+        console.error('[trial.js] window.TRIAL_CONFIG is not defined. ' +
+            'Did you forget to load trial-config.js before trial.js?');
+        return;
+    }
 
-    /* ==========================================================
-       TRIAL DATA
-       f1 = left flanker filename
-       cp = center picture filename
-       f2 = right flanker filename
-       ft = flanker type (neu / neg / none)
-       ========================================================== */
-    var SET1 = [
-        { trial_num: 1, f1: "2394_neut.JPG", cp: "2036_neut.jpg", f2: "2394_neut.JPG", ft: "neu" },
-        { trial_num: 2, f1: "None.jpg", cp: "2102_neut.jpg", f2: "None.jpg", ft: "none" },
-        { trial_num: 3, f1: "2700_neg.JPG", cp: "2107_neut.jpg", f2: "2700_neg.JPG", ft: "neg" },
-        { trial_num: 4, f1: "None.jpg", cp: "2214_neut.jpg", f2: "None.jpg", ft: "none" },
-        { trial_num: 5, f1: "2276_neg.JPG", cp: "2308_neut.jpg", f2: "2276_neg.JPG", ft: "neg" },
-        { trial_num: 6, f1: "None.jpg", cp: "2382_neut.jpg", f2: "None.jpg", ft: "none" },
-        { trial_num: 7, f1: "2359_neut.jpg", cp: "2390_neut.jpg", f2: "2359_neut.jpg", ft: "neu" },
-        { trial_num: 8, f1: "4605_neut.JPG", cp: "2393_neut.JPG", f2: "4605_neut.JPG", ft: "neu" },
-        { trial_num: 9, f1: "None.jpg", cp: "2396_neut.jpg", f2: "None.jpg", ft: "none" },
-        { trial_num: 10, f1: "6831_neg.bmp", cp: "2397_neut.jpg", f2: "6831_neg.bmp", ft: "neg" },
-        { trial_num: 11, f1: "None.jpg", cp: "2411_neut.jpg", f2: "None.jpg", ft: "none" },
-        { trial_num: 12, f1: "None.jpg", cp: "2435_neut.JPG", f2: "None.jpg", ft: "none" },
-        { trial_num: 13, f1: "2305_neut.jpg", cp: "2488_neut.jpg", f2: "2305_neut.jpg", ft: "neu" },
-        { trial_num: 14, f1: "2026_neut.jpg", cp: "2489_neut.jpg", f2: "2026_neut.jpg", ft: "neu" },
-        { trial_num: 15, f1: "7493_neut.JPG", cp: "2495_neut.JPG", f2: "7493_neut.JPG", ft: "neu" },
-        { trial_num: 16, f1: "3550_neg.bmp", cp: "2514_neut.JPG", f2: "3550_neg.bmp", ft: "neg" },
-        { trial_num: 17, f1: "None.jpg", cp: "2518_neut.JPG", f2: "None.jpg", ft: "none" },
-        { trial_num: 18, f1: "2273_neut.jpg", cp: "2570_neut.JPG", f2: "2273_neut.jpg", ft: "neu" },
-        { trial_num: 19, f1: "2749_neut.JPG", cp: "2579_neut.JPG", f2: "2749_neut.JPG", ft: "neu" },
-        { trial_num: 20, f1: "None.jpg", cp: "2580_neut.JPG", f2: "None.jpg", ft: "none" },
-        { trial_num: 21, f1: "6571_neg.JPG", cp: "2593_neut.jpg", f2: "6571_neg.JPG", ft: "neg" },
-        { trial_num: 22, f1: "9400_neg.bmp", cp: "2745.1_neut.jpg", f2: "9400_neg.bmp", ft: "neg" },
-        { trial_num: 23, f1: "9332_neg.jpg", cp: "2850_neut.JPG", f2: "9332_neg.jpg", ft: "neg" },
-        { trial_num: 24, f1: "9419_neg.jpg", cp: "8312_neut.jpg", f2: "9419_neg.jpg", ft: "neg" }
-    ];
+    var IMG = window.TRIAL_CONFIG.images;
+    var SET1 = window.TRIAL_CONFIG.trials;
 
     /* Set 2: same images, trial numbers 25-48 */
     var SET2 = SET1.map(function (t) {
